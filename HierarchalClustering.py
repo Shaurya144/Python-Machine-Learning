196fcab0965d16b2bc19ec0783e49a3c5cece017
Shaurya144 <chundawatshaurya@gmail.com>
2024-08-28 16:09:01 +0100

    Update HierarchalClustering.py

--- HierarchalClustering.py
+++ HierarchalClustering.py
@@ -7,8 +7,8 @@ import numpy as np
 import matplotlib.pyplot as plt
 from scipy.cluster.hierarchy import dendrogram, linkage
 
-x = [4, 5, 10, 4, 3, 11, 14 , 6, 10, 12]
-y = [21, 19, 24, 17, 16, 25, 24, 22, 21, 21]
+x = [4, 5, 10, 4, 3, 11, 14 , 6, 10, 12, 15]
+y = [21, 19, 24, 17, 16, 25, 24, 22, 21, 21, 30]
 
 data = list(zip(x, y))
 
